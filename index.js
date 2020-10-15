320be41e10ac1ce9a1ab96ada48568fa9a9e488c
Your name here <your_email_address@example.com>
2020-10-15 20:23:04 +0300

    auto_commit948

---
+++ index.js
@@ -0,0 +1,97 @@
+const readline = require('readline');
+const fs = require('fs');
+const path = require('path');
+const ROOT = path.join( __dirname, 'root');
+const chalk = require('chalk');
+const execSync = require('child_process').execSync
+
+
+
+
+
+
+const interface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+
+
+
+
+const createSnapshot = function() {
+    try {
+        console.log(chalk.green('Начинаем скнирование'));
+        const dirs = fs.readdirSync(__dirname + '/root/photos', { withFileTypes: true })
+            .filter(d => d.isDirectory())
+            .map(d => d.name);
+        
+        console.log(`${dirs.length + 1} альбомов найдено`);
+        const jsonData = {
+            albumsList: []
+        }
+        dirs.forEach((dirName) => {
+            const filesList = fs.readdirSync(ROOT + '/photos/' + dirName);
+            console.log(`Альбом: ${chalk.bold(dirName)} --- фотографий ${chalk.bold(filesList.length + 1)}`);
+            jsonData.albumsList.push({name:dirName, photosList:filesList});
+        });
+        
+        fs.writeFileSync(ROOT + '/photos/data.json', JSON.stringify(jsonData));
+    }
+    catch (e) {
+        console.error(chalk.red('Smth went wrong!'));
+    }
+}
+const createName = function() {
+    let nameString = 'auto_commit';
+    nameString += Math.floor(Math.random() * 1024);
+    return nameString;
+}
+const push = function() {
+    console.log(chalk.green('Начинаем выгрузку на сервер'));
+    try {
+        console.log(execSync(`git add . && git commit -m "${createName()}" && git push -f`).toString());
+    }
+    catch (e) {
+        console.error(chalk.yellow('Произошла не критическая ошибка\n') + e.output);
+    }
+    console.log(chalk.green.bold('Finished'));
+}
+
+
+const selfUpdate = function() {
+    console.log(chalk.green('Начинаем обновление'));
+    try {
+        execSync('git pull --force');
+        console.log(chalk.green('Обновление завершено'));
+    }
+    catch (e) {
+        console.error(chalk.red('Произошла ошибка при обновлении\n') + e.output);
+    }
+}
+
+const fix =function () {
+    try {
+        console.log(chalk.green('Решаем проблему'));
+        execSync('git fetch --all');
+        console.log(chalk.green('Проблема решена'));
+    }
+    catch (e) {
+        console.error(e);
+    }
+}
+
+interface.on('line', (line) => {
+    if (line === 'snapshot') {
+        createSnapshot();
+    }
+    else if (line === 'push') {
+        push();
+    }
+    else if (line === 'update') {
+        selfUpdate();
+    }
+    else if (line === 'fix') {
+        fix();
+    }
+});
---
+++ root/precache-manifest.1c8d3d27c7308f7b7a421bb4eadf14d1.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d0a28632c72ce0985d01e7a153cf092",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c546d585ddfe5c5ef833",
+    "url": "/static/css/main.b09dafc6.chunk.css"
+  },
+  {
+    "revision": "365aadbc94db6b28289c",
+    "url": "/static/js/2.4696840b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4696840b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c546d585ddfe5c5ef833",
+    "url": "/static/js/main.db329d86.chunk.js"
+  },
+  {
+    "revision": "16d50208a04408d1bf24",
+    "url": "/static/js/runtime-main.23041e79.js"
+  },
+  {
+    "revision": "a6705a8ba68f6d2351db313e97b4a41e",
+    "url": "/static/media/1.a6705a8b.jpg"
+  },
+  {
+    "revision": "dd5d8a03fdf01e6ebf97eb1dafabfb6b",
+    "url": "/static/media/1827px-Ilja_Jefimowitsch_Repin_-_Reply_of_the_Zaporozhian_Cossacks_-_Yorck.dd5d8a03.jpg"
+  },
+  {
+    "revision": "289ecbbf32beaeb074f1c5776b8cbb74",
+    "url": "/static/media/Arrow.289ecbbf.svg"
+  },
+  {
+    "revision": "88823c2015ffd5fa89d567e17297a137",
+    "url": "/static/media/Roboto-Light.88823c20.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "df8d807f4e4dccadc755919835314b49",
+    "url": "/static/media/close.df8d807f.svg"
+  },
+  {
+    "revision": "b7725c73a5ece425dba20f5e2c00fc2b",
+    "url": "/static/media/e13e11278a9c6cf7bde187e48da47565.b7725c73.png"
+  },
+  {
+    "revision": "343643e4ad81618eccb572efc41deca5",
+    "url": "/static/media/film-strip.343643e4.svg"
+  },
+  {
+    "revision": "ba0c77843e011ee4d5d2fb7855268640",
+    "url": "/static/media/photo-file.ba0c7784.svg"
+  },
+  {
+    "revision": "50863b5758b27a181975fb2f41aa1126",
+    "url": "/static/media/rubo-franc-kazaki-u-gornoy.50863b57.jpg"
+  }
+]);
